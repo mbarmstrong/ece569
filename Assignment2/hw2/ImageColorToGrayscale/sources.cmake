1730e861185131cd9393fcdd92051c1e9a17c978
mbarmstrong <58150046+mbarmstrong@users.noreply.github.com>
2021-02-07 17:57:06 -0700

    Delete Assignment2 directory

--- Assignment2/hw2/ImageColorToGrayscale/sources.cmake
+++
@@ -1,3 +0,0 @@
-add_lab("ImageColorToGrayscale")
-add_lab_solution("ImageColorToGrayscale" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("ImageColorToGrayscale" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
--- Assignment2/hw2/sources.cmake
+++
@@ -1,2 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/VectorAdd/sources.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/ImageColorToGrayscale/sources.cmake)
