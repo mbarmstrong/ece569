a51e647fbb8e552e08dc1485f2dd50f2da24fc8d
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-04 16:51:58 -0700

    com

--- labs/hw3/TiledMatrixMultiplication/solution_template_with_timer_utility_without_wb_library.cu
+++ labs/hw3/TiledMatrixMultiplication/solution_template_with_timer_utility_without_wb_library.cu
@@ -11,7 +11,7 @@
     }                                                                     \
   } while (0)
 
-#define TILE_WIDTH 4
+#define TILE_WIDTH 16
 
 // Compute C = A * B
 __global__ void matrixMultiplyShared(float *A, float *B, float *C, int numARows, int numAColumns, int numBRows, int numBColumns, int numCRows, int numCColumns) {
--- labs/hw3/TiledMatrixMultiplication/sources.cmake
+++ labs/hw3/TiledMatrixMultiplication/sources.cmake
@@ -1,4 +1,4 @@
 
 add_lab("TiledMatrixMultiplication")
-add_lab_solution("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
+add_lab_solution("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/solution_template_with_timer_utility_without_wb_library.cu)
 add_generator("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
