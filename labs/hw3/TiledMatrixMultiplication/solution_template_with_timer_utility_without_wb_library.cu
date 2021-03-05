a1f495ce8a0d2870719bb9941d46a0d30c1ff8ae
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-04 17:16:04 -0700

    com

--- labs/hw3/TiledMatrixMultiplication/solution_template_with_timer_utility_without_wb_library.cu
+++ labs/hw3/TiledMatrixMultiplication/solution_template_with_timer_utility_without_wb_library.cu
@@ -11,7 +11,7 @@
     }                                                                     \
   } while (0)
 
-#define TILE_WIDTH 32
+#define TILE_WIDTH 2
 
 // Compute C = A * B
 __global__ void matrixMultiplyShared(float *A, float *B, float *C, int numARows, int numAColumns, int numBRows, int numBColumns, int numCRows, int numCColumns) {
