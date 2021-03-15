9e47b6f0dba479a6d02ee3f979c7e8a2a3465775
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-14 19:49:21 -0700

    com

--- labs/hw4/Histogram/kernel.cu
+++ labs/hw4/Histogram/kernel.cu
@@ -67,46 +67,46 @@ __global__ void histogram_shared_accumulate_kernel(unsigned int *input, unsigned
                                  unsigned int num_bins) {
 
 	// insert your code here
-	int i = threadIdx.x + blockIdx.x * blockDim.x; // index
-	int stride = blockDim.x * gridDim.x; // total number of threads
-	__shared__ unsigned int bins_private[4096]; // privatized bins
-	unsigned int temp;
+	// int i = threadIdx.x + blockIdx.x * blockDim.x; // index
+	// int stride = blockDim.x * gridDim.x; // total number of threads
+	// // __shared__ unsigned int bins_private[4096]; // privatized bins
+	// unsigned int temp;
 
-	for (int i = 0; i < num_elements; i++) {
-		input[i] = input[i];
-	}
+	// for (int i = 0; i < num_elements; i++) {
+	// 	input[i] = input[i];
+	// }
 
-	// bubble sort
-	for (int i = 0; i < num_elements/2; i++) {
-	    int j = threadIdx.x;
-	    if (j % 2 == 0 && j < num_elements-1) {
-	        if (input[j+1] < input[j]) {
-	        	temp = input[j+1];
-	        	input[j+1] = input[j];
-	        	input[j] = temp;
-	            // swap(input[j+1], input[j]);
-	        }
-	    }
-	    __syncthreads();
+	// // bubble sort
+	// for (int i = 0; i < num_elements/2; i++) {
+	//     int j = threadIdx.x;
+	//     if (j % 2 == 0 && j < num_elements-1) {
+	//         if (input[j+1] < input[j]) {
+	//         	temp = input[j+1];
+	//         	input[j+1] = input[j];
+	//         	input[j] = temp;
+	//             // swap(input[j+1], input[j]);
+	//         }
+	//     }
+	//     __syncthreads();
 
-	    if (j % 2 == 1 && j < num_elements-1) {
-	        if (input[j+1] < input[j]) {
-	        	temp = input[j+1];
-	        	input[j+1] = input[j];
-	        	input[j] = temp;
-	            // swap(input[j+1], input[j]);
-	        }
-	    }
-	    __syncthreads();
-	}
+	//     if (j % 2 == 1 && j < num_elements-1) {
+	//         if (input[j+1] < input[j]) {
+	//         	temp = input[j+1];
+	//         	input[j+1] = input[j];
+	//         	input[j] = temp;
+	//             // swap(input[j+1], input[j]);
+	//         }
+	//     }
+	//     __syncthreads();
+	// }
 
-	i = 0;
-	while (i < num_elements) {
-		int pos = input[i]; // bin position
-		if (pos >= 0 && pos < num_bins) // boundary condition check
-			atomicAdd(&bins[pos], 1); // atomically increment appropriate bin
-		i += stride;
-	}
+	// i = 0;
+	// while (i < num_elements) {
+	// 	int pos = input[i]; // bin position
+	// 	if (pos >= 0 && pos < num_bins) // boundary condition check
+	// 		atomicAdd(&bins[pos], 1); // atomically increment appropriate bin
+	// 	i += stride;
+	// }
 
 	// sorting based approach
 	// reduce by key
