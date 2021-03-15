a3f2239f77879d2cf358738b0e2747e956b4f467
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-15 16:29:51 -0700

    com

--- labs/hw4/Histogram/kernel.cu
+++ labs/hw4/Histogram/kernel.cu
@@ -76,7 +76,7 @@ __global__ void histogram_shared_accumulate_kernel(unsigned int *input, unsigned
 	// int stride = blockDim.x * gridDim.x; // total number of threads
 	// __shared__ unsigned int bins_private[4096]; // privatized bins
 
-	thrust::device_vector<unsigned int> input_sort = input; // copy input data
+	thrust::device_vector<unsigned int*> input_sort = input; // copy input data
 	thrust::sort(input_sort.begin(), input_sort.end()); // sort input 
 
 	// thrust::device_vector<unsigned int> histo_values;
