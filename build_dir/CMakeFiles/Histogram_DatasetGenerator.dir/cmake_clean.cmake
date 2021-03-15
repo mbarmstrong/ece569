067d76d1bfab4ea817cb2a5d31280308ac91830b
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-14 18:25:51 -0700

    com

---
+++ build_dir/CMakeFiles/Histogram_DatasetGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Histogram_DatasetGenerator.dir/hw4/Histogram/dataset_generator.cpp.o"
+  "Histogram_DatasetGenerator.pdb"
+  "Histogram_DatasetGenerator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Histogram_DatasetGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
