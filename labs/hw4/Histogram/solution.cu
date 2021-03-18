b6d14190fea09ecdc2bd92c0b9984166907f7539
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-17 20:19:38 -0700

    com

--- labs/hw4/Histogram/solution.cu
+++ labs/hw4/Histogram/solution.cu
@@ -167,11 +167,11 @@ int main(int argc, char *argv[]) {
   	thrust::sort(thrust::device, input_thrust.begin(), input_thrust.end()); // sort input vector
 
   	// thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
-  												thrust::constant_iterator<int>(1), bins_thrust.begin(), lengths_thrust.begin());
+  												// thrust::constant_iterator<int>(1), bins_thrust.begin(), lengths_thrust.begin());
 
   	thrust::upper_bound(thrust::device,
                         input_thrust.begin(),input_thrust.end(),
-                        binwidths_thrust.begin(),binwidths_thrust.end(),
+                        lengths_thrust.begin(),lengths_thrust.end(),
                         bins_thrust.begin());
 
     thrust::adjacent_difference(thrust::device,
