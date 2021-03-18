b8307540152e5ffced39cae1a79f6db45dec2983
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-17 20:08:30 -0700

    com

--- labs/hw4/Histogram/solution.cu
+++ labs/hw4/Histogram/solution.cu
@@ -7,18 +7,16 @@
 #include<stdlib.h>
 #include <wb.h>
 #include "kernel.cu"
+#define NUM_BINS 4096
 
-#include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <thrust/execution_policy.h>
+#include <thrust/host_vector.h>
 #include <thrust/copy.h>
 #include <thrust/sort.h>
+#include <thrust/transform.h>
 #include <thrust/binary_search.h>
 #include <thrust/adjacent_difference.h>
-#include <thrust/transform.h>
-
-
-#define NUM_BINS 4096
+#include <thrust/execution_policy.h>
 
 #define CUDA_CHECK(ans)                                                   \
   { gpuAssert((ans), __FILE__, __LINE__); }
@@ -31,30 +29,6 @@ inline void gpuAssert(cudaError_t code, const char *file, int line,
       exit(code);
   }
 }
-
- template<typename T>
- struct greater_127
- {
-   typedef T argument_type;
- 
-   typedef bool result_type;
- 
-   __thrust_exec_check_disable__
-   __host__ __device__ bool operator()(const T &lhs) const {return lhs > 127;}
- }; // end greater_127
-
-template<typename T>
- struct set_equal_127
- {
-   typedef T argument_type;
- 
-   typedef T result_type;
- 
-   __thrust_exec_check_disable__
-   __host__ __device__ T operator()(const T &x) const {return 127;}
- }; // end set_equal_127
-
-
 
 void histogram(unsigned int *input, unsigned int *bins,
                unsigned int num_elements, unsigned int num_bins, int kernel_version) {
@@ -126,6 +100,8 @@ else if (kernel_version==2) {
     CUDA_CHECK(cudaDeviceSynchronize());
   }
  }
+
+
 }
 
 int main(int argc, char *argv[]) {
@@ -170,60 +146,32 @@ int main(int argc, char *argv[]) {
   CUDA_CHECK(cudaDeviceSynchronize());
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
-  version = atoi(argv[5]);
-
-  // ----------------------------------------------------------
-  // Use thrust library
-  // ----------------------------------------------------------
-  if(version == 3){
-    unsigned int *binwidths;
-    binwidths = (unsigned int *)malloc((NUM_BINS) * sizeof(unsigned int));
-
-    for(int i = 0; i<(NUM_BINS); i++){
-        binwidths[i] = i;
-    }
-
-    thrust::device_vector<unsigned int>input_thrust(hostInput, hostInput + inputLength);
-    thrust::device_vector<unsigned int>binwidths_thrust(binwidths, binwidths + NUM_BINS);
-    thrust::device_vector<unsigned int>bins_thrust(NUM_BINS);
-
-    cudaEventRecord(astartEvent, 0);
-
-    thrust::sort(thrust::device,input_thrust.begin(),input_thrust.end());
-
-    thrust::upper_bound(thrust::device,
-                        input_thrust.begin(),input_thrust.end(),
-                        binwidths_thrust.begin(),binwidths_thrust.end(),
-                        bins_thrust.begin());
-
-    thrust::adjacent_difference(thrust::device,
-                                bins_thrust.begin(), bins_thrust.end(),
-                                bins_thrust.begin());
-
-    set_equal_127<unsigned int>op;
-    greater_127<unsigned int>pred;
-    thrust::transform_if(bins_thrust.begin(), bins_thrust.end(), bins_thrust.begin(), op, pred);
-
-    cudaEventRecord(astopEvent, 0);
-    cudaEventSynchronize(astopEvent);
-    cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
-    printf("\n");
-    printf("Total compute time (ms) %f for version %d\n",aelapsedTime,version);
-    printf("\n");
-
-    thrust::copy(bins_thrust.begin(), bins_thrust.end(), hostBins);
-
-  }
-  // ----------------------------------------------------------
-  //  or launch kernels
+  // Launch kernel
   // ----------------------------------------------------------
   // wbTime_start(Compute, "Performing CUDA computation");
-  else {
-    cudaEventRecord(astartEvent, 0);
-    histogram(deviceInput, deviceBins, inputLength, NUM_BINS,version);
-    // wbTime_stop(Compute, "Performing CUDA computation");
 
-    cudaEventRecord(astopEvent, 0);
+  version = atoi(argv[5]);
+
+  if (version == 2) {	// thrust version
+  	cudaEventRecord(astartEvent, 0);
+
+		unsigned int *lengths;
+		lengths = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
+
+		for (int i = 0; i < NUM_BINS; i++) lengths[i] = i;
+
+  	thrust::device_vector<unsigned int> input_thrust(hostInput, hostInput + inputLength);
+  	thrust::device_vector<unsigned int> bins_thrust(NUM_BINS);
+  	thrust::device_vector<unsigned int> lengths_thrust(lengths, lengths + NUM_BINS);
+
+  	thrust::sort(thrust::device, input_thrust.begin(), input_thrust.end()); // sort input vector
+
+  	thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
+  												thrust::constant_iterator<int>(1), bins_thrust.begin(), lengths.begin());
+
+  	// thrust::transform_if();
+
+ 		cudaEventRecord(astopEvent, 0);
     cudaEventSynchronize(astopEvent);
     cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
     printf("\n");
@@ -231,15 +179,31 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     wbTime_start(Copy, "Copying output memory to the CPU");
-    //@@ Copy the GPU memory back to the CPU here
-    CUDA_CHECK(cudaMemcpy(hostBins, deviceBins,
-                          NUM_BINS * sizeof(unsigned int),
-                          cudaMemcpyDeviceToHost));
-    CUDA_CHECK(cudaDeviceSynchronize());
+    
+    thrust::copy(bins_thrust.begin(), bins_thrust.end(), hostBins); // thrust copy
+    
     wbTime_stop(Copy, "Copying output memory to the CPU");
-  }
+  } 
 
-  // ----------------------------------------------------------
+  cudaEventRecord(astartEvent, 0);
+  histogram(deviceInput, deviceBins, inputLength, NUM_BINS,version);
+  // wbTime_stop(Compute, "Performing CUDA computation");
+
+  cudaEventRecord(astopEvent, 0);
+  cudaEventSynchronize(astopEvent);
+  cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+  printf("\n");
+  printf("Total compute time (ms) %f for version %d\n",aelapsedTime,version);
+  printf("\n");
+
+  wbTime_start(Copy, "Copying output memory to the CPU");
+  //@@ Copy the GPU memory back to the CPU here
+  CUDA_CHECK(cudaMemcpy(hostBins, deviceBins,
+                        NUM_BINS * sizeof(unsigned int),
+                        cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaDeviceSynchronize());
+  wbTime_stop(Copy, "Copying output memory to the CPU");
+
   // Verify correctness
   // -----------------------------------------------------
   printf ("running version %d\n", version);
