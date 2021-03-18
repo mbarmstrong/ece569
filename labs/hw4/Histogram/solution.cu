c73b49ba0066f06e0dec155d5931eb8d790f7409
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-17 22:11:49 -0700

    com

--- labs/hw4/Histogram/solution.cu
+++ labs/hw4/Histogram/solution.cu
@@ -188,19 +188,19 @@ int main(int argc, char *argv[]) {
 
   	thrust::sort(thrust::device, input_thrust.begin(), input_thrust.end()); // sort input vector
 
-  	// thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
-  	// 											thrust::constant_iterator<int>(1), bins_thrust.begin(), lengths_thrust.begin());
+  	thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
+  												thrust::constant_iterator<int>(1), lengths_thrust.begin(), bins_thrust.begin());
 
-  	// find upper bounds of consecutive keys as indices (partition function)
-  	thrust::upper_bound(thrust::device,
-                        input_thrust.begin(),input_thrust.end(),
-                        lengths_thrust.begin(),lengths_thrust.end(),
-                        bins_thrust.begin());
+  	// // find upper bounds of consecutive keys as indices (partition function)
+  	// thrust::upper_bound(thrust::device,
+   //                      input_thrust.begin(),input_thrust.end(),
+   //                      lengths_thrust.begin(),lengths_thrust.end(),
+   //                      bins_thrust.begin());
 
-  	// compute the histogram by taking differences of the partition function (cumulative histogram)
-    thrust::adjacent_difference(thrust::device,
-                                bins_thrust.begin(), bins_thrust.end(),
-                                bins_thrust.begin());
+  	// // compute the histogram by taking differences of the partition function (cumulative histogram)
+   //  thrust::adjacent_difference(thrust::device,
+   //                              bins_thrust.begin(), bins_thrust.end(),
+   //                              bins_thrust.begin());
 
     // clipping function for bins > 127
     out_of_bounds<unsigned int> clip_predicate;
