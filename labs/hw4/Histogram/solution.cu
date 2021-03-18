8a0b34d4cc005771566f5af26c07f93e6b7d7851
Madeleine Armstrong <mbarmstrong@email.arizona.edu>
2021-03-17 21:09:46 -0700

    com

--- labs/hw4/Histogram/solution.cu
+++ labs/hw4/Histogram/solution.cu
@@ -104,13 +104,6 @@ void histogram(unsigned int *input, unsigned int *bins,
 
 }
 
-struct test_sumfun2 {
-__device__ __host__
-int operator() (int res1, int res2) const{
-return res1 + res2;
-  }
-};
-
 int main(int argc, char *argv[]) {
   wbArg_t args;
   int inputLength;
@@ -176,22 +169,19 @@ int main(int argc, char *argv[]) {
 
   	thrust::sort(thrust::device, input_thrust.begin(), input_thrust.end()); // sort input vector
 
-  	thrust::equal_to<int> binary_pred;
-  	test_sumfun2 mysumfun;
-  	thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
-  												thrust::constant_iterator<int>(1), bins_thrust.begin(), 
-  												lengths_thrust.begin(), binary_pred, mysumfun);
+  	// thrust::reduce_by_key(thrust::device, input_thrust.begin(), input_thrust.end(), 
+  												thrust::constant_iterator<int>(1), bins_thrust.begin(), lengths_thrust.begin());
 
-  	// // find where are the upper bounds of consecutive keys as indices (partition function)
-  	// thrust::upper_bound(thrust::device,
-   //                      input_thrust.begin(),input_thrust.end(),
-   //                      lengths_thrust.begin(),lengths_thrust.end(),
-   //                      bins_thrust.begin());
+  	// find where are the upper bounds of consecutive keys as indices (partition function)
+  	thrust::upper_bound(thrust::device,
+                        input_thrust.begin(),input_thrust.end(),
+                        lengths_thrust.begin(),lengths_thrust.end(),
+                        bins_thrust.begin());
 
-  	// // compute the histogram by taking differences of the partition function (cumulative histogram)
-   //  thrust::adjacent_difference(thrust::device,
-   //                              bins_thrust.begin(), bins_thrust.end(),
-   //                              bins_thrust.begin());
+  	// compute the histogram by taking differences of the partition function (cumulative histogram)
+    thrust::adjacent_difference(thrust::device,
+                                bins_thrust.begin(), bins_thrust.end(),
+                                bins_thrust.begin());
 
  		cudaEventRecord(astopEvent, 0);
     cudaEventSynchronize(astopEvent);
